ca602207f5e047c60760aff69bee4a423b128bd0
JackeyLea <1768478912@qq.com>
2020-01-11 22:15:08 +0800

    添加十六进制转换功能

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -39,7 +39,7 @@ void MainWindow::drawText(QString text,int size)
 
     QFont font;
     font.setFamily("SimSun"); //设置字体，宋体
-    font.setPixelSize(size);    //设置字号16,以像素为单位
+    font.setPixelSize(size);    //设置字号,以像素为单位
     font.setWeight(50);       //设置字型,不加粗
     font.setItalic(false);    //设置字型,不倾斜
     font.setUnderline(false); //设置字型,无下划线
@@ -47,27 +47,47 @@ void MainWindow::drawText(QString text,int size)
     int flats = Qt::AlignCenter |Qt::AlignTop;
     painter.drawText(0,0,size,size,flats,text);
 
+    //绘制表格
+//    font.setPixelSize(1);
+//    painter.setFont(font);
+//    painter.setPen(Qt::green);
+//    for(int i=0;i<size;i++){
+//        painter.drawLine(i,0,i,size);
+//    }
+//    for(int i=0;i<size;i++){
+//        painter.drawLine(0,i,size,i);
+//    }
+
+    int count = 0;
     QImage img = image;
-    for(int i=0;i<image.height();i++){
+    for(int i=0;i<size;i++){
         QByteArray byte;
-        for(int j=0;j<image.width();j++){
+        for(int j=0;j<size;j++){
             QRgb k=image.pixel(j,i);
             if(k!=backColor){
                 byte.append('1');
             }
             else byte.append('0');
+            if((j+1)%8==0){
+                qDebug()<<count++;
+                qDebug()<<byte;
+                byte.clear();
+            }
         }
-        qDebug()<<toHex(byte);
     }
 
     ui->labelFont->setPixmap(QPixmap::fromImage(img).scaled(ui->labelFont->size()));
 }
-
+//将QByteArray转换为十六进制字符串
 QString MainWindow::toHex(QByteArray byte)
 {
-    QString result;
-    qDebug()<<byte.count();
-    return result;
+    int temp =1;
+    int sum=0;
+    for(int i=7;i>=0;i--){
+        sum=sum+(byte.at(i)-'0')*temp;
+        temp*=2;
+    }
+    return "0x"+QString::number(sum);
 }
 
 void MainWindow::on_textText_textChanged()
@@ -82,9 +102,11 @@ void MainWindow::on_textText_textChanged()
 void MainWindow::on_comboBoxW_activated(const QString &text)
 {
     ui->comboBoxH->setCurrentText(text);
+    if(!ui->textText->toPlainText().isEmpty()) on_textText_textChanged();
 }
 
 void MainWindow::on_comboBoxH_activated(const QString &text)
 {
     ui->comboBoxW->setCurrentText(text);
+    if(!ui->textText->toPlainText().isEmpty()) on_textText_textChanged();
 }
